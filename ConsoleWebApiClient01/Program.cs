15127562dde6cf692c80e34cdd9cc13dcde87636
Aydın Türker <doktoralban@outlook.com>
2020-12-28 15:18:09 +0300

    birden dazla metod çağırma eklendi
    ayrıca utils ve usermodel class larınıda ekledim.

--- ConsoleWebApiClient01/Program.cs
+++ ConsoleWebApiClient01/Program.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿//using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+ 
 
 namespace ConsoleWebApiClient01
 {
@@ -12,30 +17,114 @@ namespace ConsoleWebApiClient01
 
         static async Task Main(string[] args)
         {
-            var urun = await EnUcuzUrun();             
-                Console.WriteLine(string.Format("ID : {0} ", urun.ProductID));
-                Console.WriteLine(string.Format("NAME : {0} ", urun.ProductName));
-                Console.WriteLine(string.Format("PRICE : {0} ", urun.UnitPrice));
-              
-                Console.WriteLine("-------------------------------------------");
- 
-            Console.ReadLine();
-        }
+            Uri url = new Uri("http://192.168.137.1/WebApiNW/Urunler/UretimdenKaldirilanlar?dsApiPass=1234");
 
-        private static async Task<Product> EnUcuzUrun()
-        {
+
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             client.DefaultRequestHeaders.Add("User-Agent", "aydin");
 
-            var strmUrun = client.GetStreamAsync("http://192.168.137.1/WebApiNW/EnUcuz");
+            UserModel usr = new UserModel();
+            usr.KulaniciAdi = "a.turker";
+            usr.KulaniciAdi = "1234"; 
+            var contentUser = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(usr), Encoding.UTF8, "application/json");
+             var ctn = await client.PostAsync(url.ToString(), contentUser);
 
-            Product urun = await JsonSerializer.DeserializeAsync<Product>(await strmUrun);
+            var result = await ctn.Content.ReadAsStreamAsync();
 
+            List<Product> Urunler = Utils.Deserialize<List<Product>>(result);
+
+            foreach (var urun in Urunler)
+            {
+                Console.WriteLine(string.Format("ID : {0} ", urun.ProductID));
+                Console.WriteLine(string.Format("NAME : {0} ", urun.ProductName));
+                Console.WriteLine(string.Format("PRICE : {0} ", urun.UnitPrice));
+
+                Console.WriteLine("-------------------------------------------");
+            } 
  
-            return urun;
-        } 
+            Console.ReadLine();
+        }
+
+
+
+        #region" En ucuz ürün "
+
+
+        //   static async Task Main(string[] args)
+        //{
+
+        //    var urun = await EnUcuzUrun();
+        //    Console.WriteLine(string.Format("ID : {0} ", urun.ProductID));
+        //    Console.WriteLine(string.Format("NAME : {0} ", urun.ProductName));
+        //    Console.WriteLine(string.Format("PRICE : {0} ", urun.UnitPrice));
+        //}
+
+
+
+        //private static async Task<Product> EnUcuzUrun()
+        //{
+        //    client.DefaultRequestHeaders.Accept.Clear();
+        //    client.DefaultRequestHeaders.Accept.Add(
+        //        new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+        //    client.DefaultRequestHeaders.Add("User-Agent", "aydin");
+
+        //    var strmUrun = client.GetStreamAsync("http://192.168.137.1/WebApiNW/EnUcuz");
+
+        //    Product urun = await JsonSerializer.DeserializeAsync<Product>(await strmUrun);
+
+
+        //    return urun;
+        //}
+
+        #endregion
+
+
+        #region" Stokta Kalmayanlar "
+        //static async Task Main(string[] args)
+        //{
+        //    Uri url = new Uri("http://localhost:51062/Urunler/StoktaKalmayanlar?dsApiPass=1234");
+
+
+        //    client.DefaultRequestHeaders.Accept.Clear();
+        //    client.DefaultRequestHeaders.Accept.Add(
+        //        new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+        //    client.DefaultRequestHeaders.Add("User-Agent", "aydin");
+
+        //    UserModel usr = new UserModel();
+        //    usr.KulaniciAdi = "a.turker";
+        //    usr.KulaniciAdi = "1234";
+
+        //    var contentUser = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(usr), Encoding.UTF8, "application/json");
+
+
+
+        //    var ctn = await client.PostAsync(url.ToString(), contentUser);
+
+        //    var result = await ctn.Content.ReadAsStreamAsync();
+
+        //    List<Product> Urunler = Utils.Deserialize<List<Product>>(result);
+
+        //    foreach (var urun in Urunler)
+        //    {
+        //        Console.WriteLine(string.Format("ID : {0} ", urun.ProductID));
+        //        Console.WriteLine(string.Format("NAME : {0} ", urun.ProductName));
+        //        Console.WriteLine(string.Format("PRICE : {0} ", urun.UnitPrice));
+
+        //        Console.WriteLine("-------------------------------------------");
+        //    }
+
+
+
+
+
+        //    Console.ReadLine();
+        //}
+
+
+        #endregion
+
 
     }
 }
---
+++ ConsoleWebApiClient01/Utils.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWebApiClient01
+{
+  public static  class Utils
+    {
+
+
+        public static void Serialize(object value, Stream s)
+        {
+            using (StreamWriter writer = new StreamWriter(s))
+            using (JsonTextWriter jsonWriter = new JsonTextWriter(writer))
+            {
+                JsonSerializer ser = new JsonSerializer();
+                ser.Serialize(jsonWriter, value);
+                jsonWriter.Flush();
+            }
+        }
+
+        public static T Deserialize<T>(Stream s)
+        {
+            using (StreamReader reader = new StreamReader(s))
+            using (JsonTextReader jsonReader = new JsonTextReader(reader))
+            {
+                JsonSerializer ser = new JsonSerializer();
+                return ser.Deserialize<T>(jsonReader);
+            }
+        }
+
+
+
+
+    }
+}
